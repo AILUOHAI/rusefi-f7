d4bba3d52603af3ad8aa77770b9c5eededc67984
AILUOHAI <info@ailuohai-tech.com>
2026-08-25 01:13:26 +0300

    syn_2_trigger_toyota.cpp

--- firmware/controllers/trigger/decoders/trigger_toyota.cpp
+++ firmware/controllers/trigger/decoders/trigger_toyota.cpp
@@ -102,7 +102,7 @@ void initialize_3GRfSE_CAM(TriggerWaveform* s) {
     //s->setTriggerSynchronizationGap3(/*gapIndex*/0, /*from*/1.1, 3.0);
 	  //s->setTriggerSynchronizationGap3(/*gapIndex*/1, /*from*/0.24, 0.9);
 
-    s->setTriggerSynchronizationGap(/*from*/0.32f, /*to*/0.60f);
+    s->setTriggerSynchronizationGap(0.45f);
 
     // Все углы повернуты на +80°, чтобы последний FALL был ровно на 360°.
     s->addEvent360(90,  TriggerValue::RISE, TriggerWheel::T_PRIMARY);
